6ef1aec1481a24803544d0bf3342f3cebda3eded
Shaffiulla Khan <khanshaffiulla@gmail.com>
2025-07-10 22:27:17 +0400

    Added HomeScreen Component

---
+++ PetCare/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container} />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default HomeScreen;
