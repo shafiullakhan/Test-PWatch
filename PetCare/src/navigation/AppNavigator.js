d256a95a5e129a79fe2d19a4bb9b9f47e7a05a6e
Shaffiulla Khan <khanshaffiulla@gmail.com>
2025-07-11 00:47:42 +0400

    Added MapView

--- PetCare/src/navigation/AppNavigator.js
+++ PetCare/src/navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import PetDetailScreen from '../screens/PetDetailScreen';
+import MapScreen from '../screens/MapScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ const AppNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="PetDetail" component={PetDetailScreen} />
+      <Stack.Screen name="Map" component={MapScreen} />
     </Stack.Navigator>
   );
 };
